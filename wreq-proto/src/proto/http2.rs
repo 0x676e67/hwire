587b44bbb61fd8619779e7dea381e60911eac9be
0x676e67 <gngppz@gmail.com>
2026-09-17 00:36:06 +0800

    docs(client): fix HTTP/2 max concurrent stream link to spec (#80)
    Backport adaptation: apply the unchanged URL correction to the local
    HTTP/2 options builder, where max_concurrent_streams is defined.
    
    Co-authored-by: dentiny <dentinyhao@gmail.com>

--- wreq-proto/src/proto/http2.rs
+++ wreq-proto/src/proto/http2.rs
@@ -562,7 +562,7 @@ impl Http2OptionsBuilder {
     ///
     /// See [Section 5.1.2] in the HTTP/2 spec for more details.
     ///
-    /// [Section 5.1.2]: https://http2.github.io/http2-spec/#rfc.section.5.1.2
+    /// [Section 5.1.2]: https://httpwg.org/specs/rfc7540.html#rfc.section.5.1.2
     #[inline]
     pub fn max_concurrent_streams(mut self, max: impl Into<Option<u32>>) -> Self {
         if let Some(max) = max.into() {
